780eaa4e8e37bb28df2545a5f2e36546b92371c4
JezzaR The Proto <jeremyruss43@gmail.com>
2020-05-16 00:02:28 +0100

    Initial commit

---
+++ Assets/Scripts/gunScript.cs
@@ -0,0 +1,35 @@
+﻿
+using UnityEngine;
+
+public class gunScript : MonoBehaviour {
+
+    public float damage = 10f;
+    public float range = 100f;
+    public float shootingSpeed = 10f;
+
+    public Camera fpsCam;
+    public ParticleSystem muzzleFlash;
+    public Rigidbody bullet;
+
+    // Update is called once per frame
+    void Update() {
+        if(Input.GetButtonDown("Fire1")){
+            shoot();
+        }
+    }
+
+    void shoot () {
+        muzzleFlash.Play();
+        RaycastHit hit;
+        Rigidbody newBullet;
+        newBullet = Instantiate(bullet, fpsCam.transform.position, Quaternion.Euler(fpsCam.transform.eulerAngles.x,fpsCam.transform.eulerAngles.y,fpsCam.transform.eulerAngles.z));
+        newBullet.AddForce(transform.forward * shootingSpeed);
+        if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range)) {
+
+            Target target = hit.transform.GetComponent<Target>();
+            if (target != null) {
+                target.TakeDamage(damage);
+            }
+        }
+    }
+}
