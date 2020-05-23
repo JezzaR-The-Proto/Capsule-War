d317aad8ce3c8604797dba192f370d432c4fcd78
JezzaR The Proto <jeremyruss43@gmail.com>
2020-05-23 09:00:57 +0100

    Rebrand - Now called Capsule War, enemies will follow you owo

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+public class EnemyAI : MonoBehaviour {
+
+    private NavMeshAgent nav;
+    private Transform player;
+    private Vector3 PlayerPos;
+
+    void Start () {
+        nav = GetComponent<NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void Update () { 
+        PlayerPos = player.position;
+        Debug.Log(PlayerPos);
+        nav.SetDestination(PlayerPos);
+    }
+}
