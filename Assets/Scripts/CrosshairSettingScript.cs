f8a6c14dc53afe1b444d83673cb98999440b24bf
JezzaR The Proto <jeremyruss43@gmail.com>
2020-05-26 15:29:00 +0100

    wooooah v0.3 done. CROSSHAIR CUSTOMISATION WOOOOOOP

---
+++ Assets/Scripts/CrosshairSettingScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CrosshairSettingScript : MonoBehaviour
+{
+    public Image Crosshair;
+    public Object[] CrosshairArray;
+    public int ArrayIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CrosshairArray = Resources.LoadAll("Crosshairs", typeof(Sprite));
+    }
+
+    public void ChangeImageForward() {
+        if (ArrayIndex + 1 != CrosshairArray.Length) {
+            ArrayIndex += 1;
+            Crosshair.sprite = (Sprite)CrosshairArray[ArrayIndex];
+        }
+    }
+
+    public void ChangeImageBackward() {
+        if (ArrayIndex != 0) {
+            ArrayIndex -= 1;
+            Crosshair.sprite = (Sprite)CrosshairArray[ArrayIndex];
+        }
+    }
+}
